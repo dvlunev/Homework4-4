9a493108bb4cae49e100e6076046cbd00ef4f3f1
dvlunev <dvlunev@ya.ru>
2023-02-01 09:04:31 +0300

    сделано ДЗ

---
+++ src/FruitWithId.java
@@ -0,0 +1,26 @@
+public class FruitWithId {
+    private final String name;
+    private final int id;
+    private static int counter = 0;
+
+    public FruitWithId(String name) {
+        this.name = name;
+        this.id = counter++;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    @Override
+    public String toString() {
+        return "FruitWithId{" +
+                "name='" + name + '\'' +
+                ", id=" + id +
+                '}';
+    }
+}
